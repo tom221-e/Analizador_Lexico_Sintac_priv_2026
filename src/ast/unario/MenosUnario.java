e7bbef9b74bb6ed31bf54797fa36b3360d710af8
TomasE <telcapo07@gmail.com>
2026-05-15 18:48:21 -0300

    unario

--- src/ast/unario/MenosUnario.java
+++ src/ast/unario/MenosUnario.java
@@ -2,33 +2,49 @@ package ast.unario;
 
 import ast.Expresion;
 import ast.OperacionUnaria;
-import ast.literal.IntLiteral;
+import parser.SymbolTable;
+import validator.ValidatorDataType;
 import llvm.CodeGeneratorHelper;
 
 public class MenosUnario extends OperacionUnaria {
-    public MenosUnario(Expresion operando) {
+    private final SymbolTable tabla; // Necesaria para el validador
+
+    public MenosUnario(Expresion operando, SymbolTable tabla) {
         super(operando);
+        this.tabla = tabla;
     }
 
     @Override
     protected String getNombreOperacion() {
-        return "- (unario)";
+        return "MENOS_UNARIO";
     }
+
     @Override
     public String generarCodigo() {
         StringBuilder resultado = new StringBuilder();
+
+        // 1. Generamos el código del operando primero (recursividad)
         resultado.append(this.operando.generarCodigo());
+
+        // 2. Usamos el validador para determinar si es INT o FLOAT
+        ValidatorDataType validator = new ValidatorDataType();
+        ValidatorDataType.InfoNodo info = validator.obtenerInfo(this.operando, this.tabla);
+
+        // 3. Solicitamos un nuevo registro para el resultado
         this.setIr_ref(CodeGeneratorHelper.getNewPointer());
 
-        if (this.operando instanceof IntLiteral) {
-            // %res = sub i32 0, %valor
+        if ("INT".equals(info.getTipo())) {
+            // Para enteros: 0 - operando
             resultado.append(String.format("%1$s = sub i32 0, %2$s\n",
-                    this.getIr_ref(), this.operando.getIr_ref()));
+                    this.getIr_ref(),
+                    this.operando.getIr_ref()));
         } else {
-            // %res = fsub float -0.0, %valor (o 0.0)
+            // Para flotantes: 0.0 - operando
             resultado.append(String.format("%1$s = fsub float 0.0, %2$s\n",
-                    this.getIr_ref(), this.operando.getIr_ref()));
+                    this.getIr_ref(),
+                    this.operando.getIr_ref()));
         }
+
         return resultado.toString();
     }
 }
