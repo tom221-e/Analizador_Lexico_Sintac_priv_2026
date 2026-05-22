3f9b9ae143fa6e6df10e5292e7ad612ed71a1e01
TomasE <telcapo07@gmail.com>
2026-05-22 20:52:39 -0300

    Cambios de Marina

---
+++ src/ast/Igual.java
@@ -0,0 +1,17 @@
+package ast;
+
+public class Igual extends OperacionBinaria {
+    public Igual(Expresion izquierda, Expresion derecha, String tipo) {
+        super(izquierda, derecha, tipo);
+    }
+
+    @Override
+    protected String getNombreOperacion() {
+        return "="; // Corresponde a Igual
+    }
+
+    @Override
+    public String get_llvm_op_code() {
+        return("igual");
+    }
+}
